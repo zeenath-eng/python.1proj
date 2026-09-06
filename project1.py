33800e2d3590324134109d99ddeae0a556893108
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-06 22:20:48 +0530

    latest

---
+++ project1.py
@@ -0,0 +1,12 @@
+import random
+choices = ["rock", "paper","scissor"]
+player = input("choose rock , paper or scissor :").lower()
+computer= random.choices(choices)
+if player == computer:
+    print("it's a tie ! both chose {player}. ")
+elif(player == "rock" and computer == "scissor") or \
+    (player == "paper" and computer == "rock") or \
+    (player == "scissor" and computer == "paper"):
+    print ("you win {player}beats {computer}")
+else:
+    print ("computer win! {computer} beats {player}")
