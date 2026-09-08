ac23002f6c64cb4fca86004c77546c31b3ee8129
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-08 12:08:15 +0530

    Add files via upload

---
+++ eleven.py
@@ -0,0 +1,20 @@
+age = 25
+income = 20000
+if age >=18 and income >=15000:
+    print ("eligible for loan")
+else:
+    print ("not eligible")
+
+age = 15
+income = 20000
+if age >=18 or income >=15000:
+    print ("eligible for loan")
+else:
+    print ("not eligible")
+
+a=True
+b=False
+if not a :
+    print("my name is a")
+if not b:
+    print("my name is b")
