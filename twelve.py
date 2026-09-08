c3bb6919fc02c621d86ade54a5b66e6b16fb1f21
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-08 15:32:02 +0530

    Add files via upload

---
+++ twelve.py
@@ -0,0 +1,18 @@
+#slicing
+str1 ="zeenath fatima"
+s1 = str1[5:10]
+print(s1)
+s1 = str1[:10]
+print(s1)
+s1 = str1[5:]
+print(s1)
+s1 = str1[:]
+print(s1)
+s1 = str1[-8:-3]
+print(s1)
+s1 = str1[2::2]
+print(s1)
+s1 = str1[::-1]
+print(s1)
+s1 = str1[:].upper()
+print(s1)
