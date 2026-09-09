d7820e141f139879fc815a28bfa492c0d508d11f
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-10 00:19:40 +0530

    Add files via upload

---
+++ eighteen.py
@@ -0,0 +1,18 @@
+#Loops
+#For loop
+colors =["blue","black","purple","orange","pink","yellow"]
+for i in colors:
+    print (i)
+
+#While loop
+i =0
+while i <=10:
+    print(i)
+    i+=1
+
+i = 0
+while not (1<=i<=100):
+    i =int(input("enter a number between 1 to 100 :"))
+    print("valid no",i)
+
+
