0facf767e8703d3ee519dce82a23ed5950cab3ae
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-11 16:57:40 +0530

    Add files via upload

---
+++ twenty5.py
@@ -0,0 +1,40 @@
+#Statistical operations on Array
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.max(x)
+print(y)
+
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.min(x)
+print(y)
+
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.sum(x)
+print(y)
+
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.mean(x)
+print(y)
+
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.median(x)
+print(y)
+
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.prod(x)
+print(y)
+
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.var(x)
+print(y)
+
+import numpy as np
+x =np.array([10,20,30,40,50])
+y= np.std(x)
+print(y)
