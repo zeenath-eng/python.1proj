47eeaed4ea2760f7eac38385d48da675ec85003c
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-11 15:40:16 +0530

    Add files via upload

---
+++ twenty3.py
@@ -0,0 +1,81 @@
+#Arithmetic Operations
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x+y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x-y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= y-x
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x*y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x@y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x/y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x//y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x**y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+z= x%y
+print(z)
+
+import numpy as np #type: ignore
+x=np.array([[1,2],
+            [3,4]])
+y=np.array([[5,6],
+            [7,8]])
+
+print(x.transpose())
+
