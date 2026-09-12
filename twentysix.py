9c28f769f8978658273b072ec288940b313e596e
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-12 17:00:51 +0530

    Add files via upload

---
+++ twentysix.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {
+    'Name':['Zeenath','faraz','arham','inaya'],
+    'Age' :[25,35,5,3]
+}
+
+df = pd.DataFrame(data)
+
+# 3. Explicitly print it so it shows up in your terminal
+print(df)
