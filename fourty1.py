dc6262460760dadc2f128dcba2172947d968a0f0
zeenath-eng <zeenathfaraz98@gmail.com>
2026-09-17 15:22:38 +0530

    Add files via upload

---
+++ fourty1.py
@@ -0,0 +1,17 @@
+x=[1,3,5,7]
+print(x)
+x.append(9)
+print(x)
+
+x=[1,3,5,7]
+print(x)
+x.extend([9,100,30])
+print(x)
+
+s1={1,2,3,4,5}
+s1.add(7)
+print(s1)
+
+s1={1,2,3,4,5}
+s1.update({8,9,10})
+print(s1)
